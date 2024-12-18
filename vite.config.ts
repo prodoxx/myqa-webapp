5fa583377918613d9c36ca7ca7b5f197bfc2d603
Reggie <reggie.escobar94@gmail.com>
2024-12-19 03:32:05 +0800

    clear

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,31 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { installGlobals } from '@remix-run/node';
-import { resolve } from 'path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+// import { resolve } from 'path';
+// import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 installGlobals();
 
 export default defineConfig({
-  ssr: {
-    noExternal: process.env.NODE_ENV === 'production' ? true : [],
-  },
-  resolve: {
-    alias: {
-      'msw/native': resolve(
-        resolve(__dirname, './node_modules/msw/lib/native/index.mjs')
-      ),
-      'msw/browser': resolve(
-        resolve(__dirname, './node_modules/msw/lib/browser/index.mjs')
-      ),
-    },
-  },
-  build: {
-    target: 'esnext',
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+  // ssr: {
+  //   noExternal: process.env.NODE_ENV === 'production' ? true : [],
+  // },
+  // resolve: {
+  //   alias: {
+  //     'msw/native': resolve(
+  //       resolve(__dirname, './node_modules/msw/lib/native/index.mjs')
+  //     ),
+  //     'msw/browser': resolve(
+  //       resolve(__dirname, './node_modules/msw/lib/browser/index.mjs')
+  //     ),
+  //   },
+  // },
+  // build: {
+  //   target: 'esnext',
+  //   commonjsOptions: {
+  //     transformMixedEsModules: true,
+  //   },
+  // },
   plugins: [
     // nodePolyfills({
     //   include:
