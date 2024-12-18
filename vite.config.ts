b15324112b0bcf1d3f7da8c591338acb5a8be757
Reggie <reggie.escobar94@gmail.com>
2024-12-18 17:48:04 +0800

    added noExternal to vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,47 @@ declare module '@remix-run/node' {
 }
 
 export default defineConfig({
+  ssr: {
+    noExternal:
+      process.env.NODE_ENV === 'production'
+        ? [
+            /^\@radix-ui/,
+            'pino',
+            'pino-pretty',
+            '@logtail/pino',
+            /^pino$/,
+            /^pino\-pretty$/,
+            /^@logtail\/pino$/,
+            'colorette',
+            'dateformat',
+            'fast-copy',
+            'fast-safe-stringify',
+            'help-me',
+            'joycon',
+            'minimist',
+            'on-exit-leak-free',
+            'pino-abstract-transport',
+            'pump',
+            'readable-stream',
+            'secure-json-parse',
+            'sonic-boom',
+            'strip-json-comments',
+            'posthog-js',
+            '@radix-ui/react-compose-refs',
+            'react-dropzone',
+            'react-currency-input-field',
+            'vaul',
+            '@project-serum/anchor',
+          ]
+        : [
+            '@radix-ui/react-compose-refs',
+            /^\@radix-ui/,
+            'react-dropzone',
+            'react-currency-input-field',
+            'vaul',
+            '@project-serum/anchor',
+          ],
+  },
   plugins: [
     nodePolyfills({ include: ['buffer'] }),
     remix({
