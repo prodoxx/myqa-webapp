492167465cc8a6d6a8c549956dee3f8df0cdb67a
Reggie <reggie.escobar94@gmail.com>
2024-12-19 03:05:22 +0800

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -35,8 +35,6 @@ export default defineConfig({
           : ['buffer', 'process'],
       globals: {
         process: process.env.NODE_ENV === 'production' ? false : 'dev',
-        Buffer: true,
-        global: true,
       },
     }),
     remix({
